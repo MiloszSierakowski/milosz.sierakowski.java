7c0ce7c9ec4a88145de6b76570f6b9d773ad0ef8
Milosz <milosz.sierakowski@wp.pl>
2022-10-31 18:39:36 +0100

    8.7 Iteration over the titles

--- kodilla-good-patterns/src/main/java/Main.java
+++ kodilla-good-patterns/src/main/java/Main.java
@@ -1,10 +1,15 @@
 import com.kodilla.good.patterns.challenges.MovieStore;
 
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
+        Map<String , List<String>> movies = movieStore.getMovies();
 
+        movies.forEach((key, value) -> value.forEach(x -> System.out.print(x + "!")));
 
     }
 }
